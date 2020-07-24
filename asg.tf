8210f1375db0f9aa29e05f057cd15976990ecbe9
errne <upapaus@gmail.com>
2020-07-24 16:06:23 +0100

    added instance profile

--- asg.tf
+++ asg.tf
@@ -1,3 +1,30 @@
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "tf_test_profile"
+  role = "${aws_iam_role.role.name}"
+}
+
+resource "aws_iam_role" "role" {
+  name = "tf_test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+
 module "asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "~> 3.0"
@@ -10,6 +37,7 @@ module "asg" {
   image_id        = "ami-0c3e74fa87d2a4227"
   key_name        = "spraktas"
   instance_type   = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
   security_groups = ["sg-0176ce5ea22452d2e"]
 
   ebs_block_device = [
